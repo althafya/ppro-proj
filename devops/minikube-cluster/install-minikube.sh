11bb2915cc91e8d9e30d0fd4d68da6d495b9cb58
althaf <althaf.ya1@gmail.com>
2022-04-21 01:00:33 +0530

    Cluster provisioning changes

---
+++ devops/minikube-cluster/install-minikube.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# author: Althaf
+# install minikube on mac OS 
+
+function install {
+    echo "--- Start Installing minikube ---"
+    # check minikube is already installed
+    minikube status |grep 'kubelet' 2> /dev/null
+    if [ $? -eq 0 ];then
+        echo "INFO: minikube is already installed!"
+        alias kubectl="minikube kubectl --"
+    else 
+        echo "INFO: Downloading minikube ..."
+        curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64
+        minikube start 
+        alias kubectl="minikube kubectl --"
+    fi
+    
+}
+
+# run 
+install 
+
+
